d9c593ba8895828cd66ae11de175cefac2cd5ee2
jhamfler <j.hamfler@gmail.com>
2014-12-01 07:37:11 +0100

    working config for the git colors

--- .conkyskripte/git-status-awesome.sh
+++ .conkyskripte/git-status-awesome.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cd ~/git/awesome/
-lines=$(git status --porcelain | wc -l)
-if [ "$lines" != "0" ]; then
+lines=$(git status --porcelain|wc -l)
+push=$(git diff --stat origin|wc -l)
+if [ "$lines" -eq "0" ]; then
+  if [ "$push"  -eq "0" ]; then
+    echo "2"
+    exit
+  fi
   echo "1"
-  exit
-fi
-push=$(git diff --stat origin)
-if [ "$push" != "" ]; then
-  echo "2"
-  exit
+else
+  echo "no commit and no push"
 fi
