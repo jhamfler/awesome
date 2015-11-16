620c17e8b66164a1fa94947f0da8dcbbea5fdf01
Johannes Hamfler <jh@z7k.de>
2015-11-16 22:44:02 +0100

    automatic screensize isnt working everytime

--- .config/awesome/start.sh
+++ .config/awesome/start.sh
@@ -1,9 +1,9 @@
-r=$(xrandr|grep VGA|grep disconnected)
-if [[ $r == "" ]]; then
-  ~/.screenlayout/rechtermonitor.sh
-else
-  ~/.screenlayout/laptop.sh
-fi
+#r=$(xrandr|grep VGA|grep disconnected)
+#if [[ $r == "" ]]; then
+#  ~/.screenlayout/rechtermonitor.sh
+#else
+#  ~/.screenlayout/laptop.sh
+#fi
 killall conky
 killall nm-applet
 nm-applet &
